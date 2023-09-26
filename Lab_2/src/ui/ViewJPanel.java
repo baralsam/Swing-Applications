18e4c301aa97976200c6526bbcf845bd823cd63f
Samiksha Sanjay Baral <baral.sam@northeastern.edu>
2023-09-26 18:24:27 -0400

    modified a string

--- Lab_2/src/ui/ViewJPanel.java
+++ Lab_2/src/ui/ViewJPanel.java
@@ -158,11 +158,11 @@ public class ViewJPanel extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnViewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViewActionPerformed
-        // TODO add your handling code here:
+         // TODO add your handling code here:
             int selectedRowIndex = tblVitals.getSelectedRow();
         
         if(selectedRowIndex<0){
-            JOptionPane.showMessageDialog(this, "Please select a row to delete.");
+            JOptionPane.showMessageDialog(this, "Please select a row to view.");
             return;
         }
         
